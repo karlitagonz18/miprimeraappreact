e090bb4194edcdd7c07b5407fec4c037c0a667d0
karlitagonz18 <https://github.com/karlitagonz18/miprimeraappreact.git>
2022-06-04 21:50:37 -0500

    subida de archivos

--- src/components/ListaNombres.jsx
+++ src/components/ListaNombres.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from 'styled-components'
 
 const ListaNombres = () => {
 
@@ -12,9 +13,10 @@ const ListaNombres = () => {
 
     return (
         <div>
-            <p>Ingresa un nombre: </p>
-            <input type="text" value= {nombre} onChange={(e) => {setNombre(e.target.value)}}></input>
-            <button onClick={guardar}>Enviar</button>
+            <Container>
+            <Header>Ingresa un nombre: <p></p>
+            <Input type="text" value= {nombre} onChange={(e) => {setNombre(e.target.value)}}></Input>
+            <Button onClick={guardar}>Enviar</Button>
             <ul>
             {
                 nombres.map((elemento, index) => {
@@ -22,8 +24,54 @@ const ListaNombres = () => {
                 })
             }
             </ul>
+            </Header>
+            </Container>
         </div>
     )
 }
+
+export const Container = styled.div`
+       width: 100%;
+       height: 100vh;
+       display: flex;
+       justify-content: center;
+       align-items: center;
+`
+
+export const Input = styled.input`
+    border-radius: 10px;
+    margin: 10px;
+    background-color: #def;
+    width: 150px;
+    padding: 5px;
+    width: 250px;
+    height: 40px;
+`
+
+const Header = styled.h1`
+   color: Black;
+   font-size: 2rem;
+   font-familily: sans-serif;
+   
+
+&:hover {
+    background-color: #33b8ff;
+    color: Black;
+    transition: 0.3s all ease;
+
+}
+`
+
+const Button = styled.button`
+background-color: #4CAF50; /* Green */
+    border: 5px;
+    margin: 10px;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+`
 
 export default ListaNombres;
